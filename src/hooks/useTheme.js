59ee4e57c75692c3c4593684beb5b806c413db9e
Abdelmalek Maskri <abdelmalekmaskri18@gmail.com>
2024-09-07 22:23:12 +0100

    added the useReducers and useContext so thatusers can change the color of thenavBar

---
+++ src/hooks/useTheme.js
@@ -0,0 +1,14 @@
+import { ThemeContext } from '../context/ThemeContext'
+import { useContext } from 'react'
+
+
+export const useTheme = () => {
+    const context = useContext(ThemeContext);
+
+    if(context === undefined){
+        throw new Error('useTheme() must be used inside a theme provider')
+    }
+
+    return context
+
+}
